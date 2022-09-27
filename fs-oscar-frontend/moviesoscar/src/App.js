046afb95bc68552c9e4f303cf3f4dfc12e62fc25
Arthur Azoubel <arthurazoubel@gmail.com>
2022-09-27 19:34:12 -0300

    frontend initiallization

---
+++ fs-oscar-frontend/moviesoscar/src/App.js
@@ -0,0 +1,15 @@
+import { Heading, Box} from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+    <Box display='flex' flexDirection='column' textAlign='center' border='2px'>
+      <Heading>I'm a Heading</Heading>
+      <Heading>I'm a Middle Text</Heading>
+    </Box>
+    </>
+  );
+}
+
+export default App;
